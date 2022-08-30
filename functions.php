73dea423d779ad63784687f4e8760caa2a045ba1
Sandeep Kumar Rai <matrixsandeep2007@gmail.com>
2022-08-30 18:30:17 +0530

    Adding Includes
    File Added:
    Engueue.php

--- functions.php
+++ functions.php
@@ -1,9 +1,12 @@
 <?php
 
-
-
 //Variable
 
 //Include
+include(get_theme_file_path('/includes/front/enqueue.php'))
 
 //Hooks
+add_action('wp_enqueue_scripts', 'u_enqueue');
+
+
+?>
---
+++ includes/front/enqueue.php
@@ -0,0 +1,4 @@
+<?php
+function u_enqueue(){
+    
+}
